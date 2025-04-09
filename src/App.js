e17a4b84b7dbb5226840fe809475cfaa530e4a70
Samwel Mpuyuk <148690527+Sammy0254@users.noreply.github.com>
2025-04-09 22:28:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Register from './pages/Register';
 const App = () => {
   return (
     <Router>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
